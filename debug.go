f047395b4fc7c89a131209147081ebe92fec3d10
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-02-27 17:59:10 +1030

    Remove {{.Func}}() functions from debug and dev builds

--- debug.go
+++ debug.go
@@ -16,14 +16,18 @@ func init() {
 	"strings"
 )
 
-{{- range $.Assets}}
+// AssetAndInfo loads and returns the asset and asset info for the
+// given name. It returns an error if the asset could not be found
+// or could not be loaded.
+func AssetAndInfo(name string) ([]byte, os.FileInfo, error) {
+	path, ok := _bindata[strings.Replace(name, "\\", "/", -1)]
+	if !ok {
+		return nil, nil, &os.PathError{Op: "open", Path: name, Err: os.ErrNotExist}
+	}
 
-// {{.Func}} reads file data from disk. It returns an error on failure.
-func {{.Func}}() ([]byte, os.FileInfo, error) {
 {{- if $.Config.Dev}}
-	path := filepath.Join(rootDir, {{printf "%q" .Name}})
-{{- else}}
-	path := {{printf "%q" .Path}}
+
+	path = filepath.Join(rootDir, path)
 {{- end}}
 
 	data, err := ioutil.ReadFile(path)
@@ -38,24 +42,16 @@ func {{.Func}}() ([]byte, os.FileInfo, error) {
 
 	return data, fi, nil
 }
-{{- end}}
 
-// AssetAndInfo loads and returns the asset and asset info for the
-// given name. It returns an error if the asset could not be found
-// or could not be loaded.
-func AssetAndInfo(name string) ([]byte, os.FileInfo, error) {
-	if f, ok := _bindata[strings.Replace(name, "\\", "/", -1)]; ok {
-		return f()
-	}
-
-	return nil, nil, &os.PathError{Op: "open", Path: name, Err: os.ErrNotExist}
-}
-
-// _bindata is a table, holding each asset generator, mapped to its name.
-var _bindata = map[string]func() ([]byte, os.FileInfo, error){
+// _bindata is a table, mapping each file to its path.
+var _bindata = map[string]string{
 {{$max := maxNameLength .Assets -}}
 {{range .Assets}}	{{printf "%q" .Name}}:
-	{{- repeat " " (sub $max (len .Name))}} {{.Func}},
+	{{- repeat " " (sub $max (len .Name))}} {{if $.Config.Dev -}}
+	{{printf "%q" .Name}}
+{{- else -}}
+	{{printf "%q" .Path}}
+{{- end}},
 {{end -}}
 }`))
 }
