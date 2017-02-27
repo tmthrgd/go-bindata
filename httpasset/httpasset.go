tmthrgd/go-bindata

commit ab1eb061d4e70e5cb837659d1cc2e8004a0123f6

Simplify parseAcceptEncoding

httpasset/httpasset.go
@@ -106,17 +106,20 @@ func (fh *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	accept := parseAcceptEncoding(r.Header.Get("Accept-Encoding"))
+	brotli, gzip := parseAcceptEncoding(r.Header.Get("Accept-Encoding"))
 
 	if ok {
 		name = fi.OriginalName()
 	}
 
-	for _, enc := range [...]struct{ name, ext string }{
-		{"br", ".br"},
-		{"gzip", ".gz"},
+	for _, enc := range [...]struct {
+		name, ext string
+		supported bool
+	}{
+		{"br", ".br", brotli},
+		{"gzip", ".gz", gzip},
 	} {
-		if _, has := accept[enc.name]; !has {
+		if !enc.supported {
 			continue
 		}
 
@@ -145,25 +148,29 @@ func isFieldSeparator(r rune) bool {
 	return r == ';' || unicode.IsSpace(r)
 }
 
-func parseAcceptEncoding(header string) (c map[string]struct{}) {
-	c = make(map[string]struct{})
-
-outer:
+func parseAcceptEncoding(header string) (brotli, gzip bool) {
 	for _, value := range strings.Split(header, ",") {
 		fields := strings.FieldsFunc(value, isFieldSeparator)
 		if len(fields) == 0 {
 			continue
 		}
 
+		supported := true
 		for _, field := range fields[1:] {
 			switch field {
 			case "q=0", "q=0.0", "q=0.00", "q=0.000",
 				"Q=0", "Q=0.0", "Q=0.00", "Q=0.000":
-				continue outer
+				supported = false
+				break
 			}
 		}
 
-		c[strings.ToLower(fields[0])] = struct{}{}
+		switch strings.ToLower(fields[0]) {
+		case "br":
+			brotli = supported
+		case "gzip":
+			gzip = supported
+		}
 	}
 
 	return

commit 7b685a970e99033c650b16ce22e803dc41aec334

Fix Content-Encoding header (broken in ab1eb06)

httpasset/httpasset.go
@@ -101,8 +101,8 @@ func (fh *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 
 		brotli, gzip := parseAcceptEncoding(r.Header.Get("Accept-Encoding"))
-		if (brotli && fh.serveCompressed(w, r, info, name, ".br", len(data))) ||
-			(gzip && fh.serveCompressed(w, r, info, name, ".gz", len(data))) {
+		if (brotli && fh.serveCompressed(w, r, info, name+".br", "br", len(data))) ||
+			(gzip && fh.serveCompressed(w, r, info, name+".gz", "gzip", len(data))) {
 			return
 		}
 	}
@@ -110,8 +110,8 @@ func (fh *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.ServeContent(w, r, info.Name(), info.ModTime(), bytes.NewReader(data))
 }
 
-func (fh *fileHandler) serveCompressed(w http.ResponseWriter, r *http.Request, info os.FileInfo, name, ext string, size int) bool {
-	data, _, err := fh.compressed(name + ext)
+func (fh *fileHandler) serveCompressed(w http.ResponseWriter, r *http.Request, info os.FileInfo, name, enc string, size int) bool {
+	data, _, err := fh.compressed(name)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return false
@@ -124,7 +124,7 @@ func (fh *fileHandler) serveCompressed(w http.ResponseWriter, r *http.Request, i
 		return false
 	}
 
-	w.Header().Set("Content-Encoding", name)
+	w.Header().Set("Content-Encoding", enc)
 	http.ServeContent(w, r, info.Name(), info.ModTime(), bytes.NewReader(data))
 	return true
 }