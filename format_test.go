1304ae3d3bcbaf90438863fd5ffb1410df666bb7
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-02-28 19:48:56 +1030

    Add benchmarks

--- format_test.go
+++ format_test.go
@@ -155,3 +155,63 @@ func TestFormatting(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkFindFiles(b *testing.B) {
+	for _, test := range testCases {
+		test := test
+		b.Run(test.name, func(b *testing.B) {
+			c := &Config{Package: "main"}
+			test.config(c)
+
+			g, err := New(c)
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			b.ResetTimer()
+
+			for n := 0; n < b.N; n++ {
+				if err = g.FindFiles("testdata", true); err != nil {
+					b.Fatal(err)
+				}
+
+				g.toc = nil
+			}
+		})
+	}
+}
+
+func BenchmarkWriteTo(b *testing.B) {
+	for _, test := range testCases {
+		test := test
+		b.Run(test.name, func(b *testing.B) {
+			c := &Config{Package: "main"}
+			test.config(c)
+
+			g, err := New(c)
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			for _, path := range testPaths {
+				if err = g.FindFiles(path.path, path.recursive); err != nil {
+					b.Fatal(err)
+				}
+			}
+
+			b.ResetTimer()
+
+			for n := 0; n < b.N; n++ {
+				if _, err = g.WriteTo(nopWriter{}); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+type nopWriter struct{}
+
+func (nopWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
