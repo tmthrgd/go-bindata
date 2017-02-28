6334a4a4619ca7a5dad2d80a6655168d2c31a544
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-02-28 19:23:52 +1030

    Move test cases and test files slices into global variables

--- format_test.go
+++ format_test.go
@@ -16,6 +16,51 @@ import (
 	"golang.org/x/tools/imports"
 )
 
+var testCases = [...]struct {
+	name   string
+	config func(*Config)
+}{
+	{"default", func(*Config) {}},
+	{"old-default", func(c *Config) {
+		c.Package = "main"
+		c.MemCopy = true
+		c.Compress = true
+		c.Metadata = true
+		c.HashLength = 16
+		// The AssetDir API currently produces
+		// wrongly formatted code. We're going
+		// to skip it for now.
+		/*c.AssetDir = true
+		c.Restore = true*/
+		c.DecompressOnce = true
+	}},
+	{"debug", func(c *Config) { c.Debug = true }},
+	{"dev", func(c *Config) { c.Dev = true }},
+	{"tags", func(c *Config) { c.Tags = "!x" }},
+	{"package", func(c *Config) { c.Package = "test" }},
+	{"prefix", func(c *Config) { c.Prefix = "testdata" }},
+	{"compress", func(c *Config) { c.Compress = true }},
+	{"copy", func(c *Config) { c.MemCopy = true }},
+	{"metadata", func(c *Config) { c.Metadata = true }},
+	{"decompress-once", func(c *Config) { c.DecompressOnce = true }},
+	{"hash-dir", func(c *Config) { c.HashFormat = DirHash; c.HashLength = 16 }},
+	{"hash-suffix", func(c *Config) { c.HashFormat = NameHashSuffix; c.HashLength = 16 }},
+	{"hash-hashext", func(c *Config) { c.HashFormat = HashWithExt; c.HashLength = 16 }},
+	{"hash-unchanged", func(c *Config) { c.HashFormat = NameUnchanged; c.HashLength = 16 }},
+	{"hash-enc-b32", func(c *Config) { c.HashEncoding = Base32Hash; c.HashFormat = DirHash; c.HashLength = 16 }},
+	{"hash-enc-b64", func(c *Config) { c.HashEncoding = Base64Hash; c.HashFormat = DirHash; c.HashLength = 16 }},
+	{"hash-key", func(c *Config) { c.HashKey = []byte{0x00, 0x11, 0x22, 0x33}; c.HashFormat = DirHash; c.HashLength = 16 }},
+}
+
+var testPaths = [...]struct {
+	path      string
+	recursive bool
+}{
+	{"testdata", true},
+	{"LICENSE", false},
+	{"README.md", false},
+}
+
 var gencode = flag.String("gencode", "", "write generated code to specified directory")
 
 func TestFormatting(t *testing.T) {
@@ -29,41 +74,7 @@ func TestFormatting(t *testing.T) {
 		}
 	}
 
-	for _, test := range [...]struct {
-		name   string
-		config func(*Config)
-	}{
-		{"default", func(*Config) {}},
-		{"old-default", func(c *Config) {
-			c.Package = "main"
-			c.MemCopy = true
-			c.Compress = true
-			c.Metadata = true
-			c.HashLength = 16
-			// The AssetDir API currently produces
-			// wrongly formatted code. We're going
-			// to skip it for now.
-			/*c.AssetDir = true
-			c.Restore = true*/
-			c.DecompressOnce = true
-		}},
-		{"debug", func(c *Config) { c.Debug = true }},
-		{"dev", func(c *Config) { c.Dev = true }},
-		{"tags", func(c *Config) { c.Tags = "!x" }},
-		{"package", func(c *Config) { c.Package = "test" }},
-		{"prefix", func(c *Config) { c.Prefix = "testdata" }},
-		{"compress", func(c *Config) { c.Compress = true }},
-		{"copy", func(c *Config) { c.MemCopy = true }},
-		{"metadata", func(c *Config) { c.Metadata = true }},
-		{"decompress-once", func(c *Config) { c.DecompressOnce = true }},
-		{"hash-dir", func(c *Config) { c.HashFormat = DirHash; c.HashLength = 16 }},
-		{"hash-suffix", func(c *Config) { c.HashFormat = NameHashSuffix; c.HashLength = 16 }},
-		{"hash-hashext", func(c *Config) { c.HashFormat = HashWithExt; c.HashLength = 16 }},
-		{"hash-unchanged", func(c *Config) { c.HashFormat = NameUnchanged; c.HashLength = 16 }},
-		{"hash-enc-b32", func(c *Config) { c.HashEncoding = Base32Hash; c.HashFormat = DirHash; c.HashLength = 16 }},
-		{"hash-enc-b64", func(c *Config) { c.HashEncoding = Base64Hash; c.HashFormat = DirHash; c.HashLength = 16 }},
-		{"hash-key", func(c *Config) { c.HashKey = []byte{0x00, 0x11, 0x22, 0x33}; c.HashFormat = DirHash; c.HashLength = 16 }},
-	} {
+	for _, test := range testCases {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
@@ -76,16 +87,10 @@ func TestFormatting(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			if err = g.FindFiles("testdata", true); err != nil {
-				t.Fatal(err)
-			}
-
-			if err = g.FindFiles("LICENSE", false); err != nil {
-				t.Fatal(err)
-			}
-
-			if err = g.FindFiles("README.md", false); err != nil {
-				t.Fatal(err)
+			for _, path := range testPaths {
+				if err = g.FindFiles(path.path, path.recursive); err != nil {
+					t.Fatal(err)
+				}
 			}
 
 			var buf bytes.Buffer
