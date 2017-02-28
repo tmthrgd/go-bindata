cc0f7a2a7906cccf8c68ddcbb95719970a0e0ef7
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-03-01 08:25:08 +1030

    Add deterministic random test cases generated w/ quick.Value

--- translate_test.go
+++ translate_test.go
@@ -6,16 +6,25 @@ package bindata
 
 import (
 	"flag"
+	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"reflect"
 	"sort"
+	"strings"
 	"testing"
+	"testing/quick"
+	"unicode"
+	"unicode/utf8"
 )
 
-var testCases = [...]struct {
+type testCase struct {
 	name string
 	opts func(*GenerateOptions)
-}{
+}
+
+var testCases = []testCase{
 	{"default", func(*GenerateOptions) {}},
 	{"old-default", func(o *GenerateOptions) {
 		o.Package = "main"
@@ -66,7 +75,81 @@ var testPaths = map[string]*FindFilesOptions{
 	"README.md":       nil,
 }
 
-var gencode = flag.String("gencode", "", "write generated code to specified directory")
+var (
+	gencode = flag.String("gencode", "", "write generated code to specified directory")
+
+	randTestCases = flag.Uint("randtests", 25, "")
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	t := reflect.TypeOf(GenerateOptions{})
+
+	for i := uint(0); i < *randTestCases; i++ {
+		rand := rand.New(rand.NewSource(int64(i)))
+
+		v, ok := quick.Value(t, rand)
+		if !ok {
+			panic("quick.Value failed")
+		}
+
+		vo := v.Addr().Interface().(*GenerateOptions)
+		vo.Package = identifier(vo.Package)
+		vo.Mode &= os.ModePerm
+		vo.HashFormat = HashFormat(int(uint(vo.HashFormat) % uint(HashWithExt+1)))
+		vo.HashEncoding = HashEncoding(int(uint(vo.HashEncoding) % uint(Base64Hash+1)))
+		// The AssetDir API currently produces
+		// wrongly formatted code. We're going
+		// to skip it for now.
+		vo.AssetDir = false
+		vo.Restore = vo.Restore && vo.AssetDir
+
+		if vo.Package == "" {
+			vo.Package = "main"
+		}
+
+		switch vo.HashEncoding {
+		case HexHash:
+			vo.HashLength %= maxHexLength
+		case Base32Hash:
+			vo.HashLength %= maxB32Length
+		case Base64Hash:
+			vo.HashLength %= maxB64Length
+		}
+
+		if vo.Debug || vo.Dev {
+			vo.HashFormat = NoHash
+		}
+
+		testCases = append(testCases, testCase{
+			fmt.Sprintf("random-#%d", i+1),
+			func(o *GenerateOptions) { *o = *vo },
+		})
+	}
+
+	os.Exit(m.Run())
+}
+
+// identifier removes all characters from a string that are not valid in
+// an identifier according to the Go Programming Language Specification.
+//
+// The logic in the switch statement was taken from go/source package:
+// https://github.com/golang/go/blob/a1a688fa0012f7ce3a37e9ac0070461fe8e3f28e/src/go/scanner/scanner.go#L257-#L271
+func identifier(val string) string {
+	return strings.TrimLeftFunc(strings.Map(func(ch rune) rune {
+		switch {
+		case 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' ||
+			ch >= utf8.RuneSelf && unicode.IsLetter(ch):
+			return ch
+		case '0' <= ch && ch <= '9' ||
+			ch >= utf8.RuneSelf && unicode.IsDigit(ch):
+			return ch
+		default:
+			return -1
+		}
+	}, val), unicode.IsDigit)
+}
 
 func testFiles() (Files, error) {
 	var all Files
