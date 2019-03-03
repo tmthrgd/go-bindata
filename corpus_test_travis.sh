6fbf6c8bd45cf2d4189179ca4e92bb7438db4a93
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-03-03 22:44:34 +1030

    Update go version in corpus_test_travis.sh

--- corpus_test_travis.sh
+++ corpus_test_travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [[ `go version` = *"go1.11"* ]]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [[ `go version` = *"go1.12"* ]]; then
 	go test -v -race -run TestCorpus -randtests 50 -corpus .travis-corpus -gencorpus .
 	cd .travis-corpus && sha256sum -c --quiet --strict ../corpus-sha256sums
 fi
